72fbf7d3c0a9bebb6933c5d94fd2c8f7ddd6e2e1
AntoineC-12 <lejoker-17@live.fr>
2015-06-06 23:16:12 +0200

    Forgot to add the file to git before my previous commit.

---
+++ game_engine.pl
@@ -0,0 +1,11 @@
+% This is the initilisation file of the StockExchange Game
+% Author: Antoine Pouillaude.
+
+use_module(stock_exchange).
+
+
+myVar(board,[['wheat ',7,6,5,4,3,2,1],['corn  ',6,5,4,3,2,1,0],['rice  ',6,5,4,3,2,1,0],['sugar ',6,5,4,3,2,1,0],['coffee',6,5,4,3,2,1,0],['cocoa ',6,5,4,3,2,1,0]]).
+
+starting_state(State).
+
+
