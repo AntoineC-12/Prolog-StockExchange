cd00b560dbe3c1d979f59b0300d671ed8fb323f9
AntoineC-12 <lejoker-17@live.fr>
2015-06-08 17:14:23 +0200

    Implemented the new predicates all_possible_moves, possible_moves, get_possible_list in the game_ai file. Those rules are here to compute all the moves possible on the current board state.

--- game_ai.pl
+++ game_ai.pl
@@ -1,4 +1,26 @@
 % This is the file containing the AI of the StockExchange Game
 % Author: Antoine Pouillaude.
 
-all_possible_moves().
+
+%% The following predicate will get all the possible moves that the ai could make.
+all_possible_moves([Stacks,S,TP,RJ1,RJ2],Player,PossibleMoves) :- possible_moves(Stacks,TP,Player,[1,2,3],PossibleMoves).
+
+
+%% possible_moves will get the list of the moves possible by applying all the jumps given in the fourth parameter. In that game the list should be [1,2,3].
+possible_moves(Stacks,TP,Player,[],[]) :- !.
+possible_moves(Stacks,TP,Player,[H|T],ListMoves) :-
+		possible_moves(Stacks,TP,Player,T,SListMoves), 
+		get_possible_list(Stacks,TP,H,ListProd),
+		reverse(ListProd,RevList),
+		concat([Player,H],ListProd, TempList),
+		concat([Player,H],RevList, TempListRev),
+		concat([TempList],[TempListRev],Temp),
+		concat(Temp,SListMoves,ListMoves).
+
+%% This predicate will compute the list of products available from the current trader position by applying Pos moves to the pawn.
+get_possible_list(Stacks,TP,Pos,ListProd) :-
+		get_indexes(Stacks,TP,Pos,[NTP,ISup,IInf]),
+		nth0(ISup,Stacks,E1),
+		nth0(IInf,Stacks,E2),
+		[Prod1|T1] = E1, [Prod2|T2] = E2,
+		ListProd = [Prod1,Prod2].
